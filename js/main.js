514332cc88e3e850dc6dbe2c9b9dc224297a1733
Ricardo Süffert <ricardobsuffert@gmail.com>
2023-05-08 00:40:19 -0300

    Adding slider buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-$('.slider-principal').slick(); // . indica classe, e # indica id. Esta atribuindo isso para o slider principal
+$('.slider-principal').slick({ // . indica classe, e # indica id. Esta atribuindo isso para o slider principal
+    // estilizando o carrossel
+    dots: true,
+    inifinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptativeHeight: true
+});
